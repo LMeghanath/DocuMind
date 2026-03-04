eac78232d39428be3e4ae8c1c9155766a444812a
Asrit <asritp5@gmail.com>
2026-03-04 10:23:21 +0530

    added login functionality

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    email=models.EmailField(unique=True)
+    document_count=models.IntegerField(default=0)
+    api_usage=models.IntegerField(default=0)
+    total_storage=models.BigIntegerField(default=0)
+    subscription_type=models.CharField(max_length=20,default="Free")
+    
+    def __str__(self):
+        return f"{self.user.name} Profile" 
+
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth  import authenticate,login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def homepage(request):
     return render(request,"mainapp/homepage.html")
+
+@login_required
+def chatpage(request):
+    return render(request,"mainapp/chatpage.html")
+
+def login(request):
+    if request.method=="POST":
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+
+        try:
+            user=user.objects.get(email=email)
+            user=authenticate(request,user.username,password)
+        except user.DoesNotExist:
+            user=None
+            messages.error("Invalid credentials!")    
+
+        if user!=None:
+            login(request,user)
+            redirect("chatpage")
+                    
+    return render(request,"mainapp/login.html")    
+
+@login_required
+def logout(request):
+    logout(request)
+    redirect("homepage")
