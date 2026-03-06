5e189260e645c7d87801756dacedda3d70403a9d
Asrit <asritp5@gmail.com>
2026-03-06 17:08:16 +0530

    Backend logic to add files and chat

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import FileExtensionValidator
 
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
@@ -11,4 +12,44 @@ class Profile(models.Model):
     
     def __str__(self):
         return f"{self.user.name} Profile" 
+
+class Document(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    file_name=models.CharField(max_length=255)
+    file=models.FileField(upload_to='user_documents/',
+                          validators=[FileExtensionValidator(allowed_extensions=["pdf"])])
+    file_size=models.IntegerField()
+    chunk_count=models.IntegerField()
+    file_hash=models.CharField(max_length=64)
+    upload_time=models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together=("user","file_hash")
+
+class Chat(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    chat_time=models.DateTimeField(auto_now_add=True)
+    chat_title=models.CharField(max_length=200,default="New Chat")
+    class Meta:
+        ordering=["-chat_time"]
+
+class Message(models.Model):
+    ROLES=[
+        ("user","User"),
+        ("assistant","Assistant"),
+        ("system","System"),
+    ]
+
+    chat=models.ForeignKey(Chat,on_delete=models.CASCADE)
+    role=models.CharField(max_length=10,choices=ROLES)
+    message_text=models.TextField()
+    message_time=models.DateTimeField(auto_now_add=True)
+
+class Chunk(models.Model):
+    document=models.ForeignKey(Document,on_delete=models.CASCADE)
+    chunk_text=models.TextField()
+    chunk_index=models.IntegerField()  
+    faiss_index=models.IntegerField(unique=True)
+    page_number=models.IntegerField()
+    class Meta:
+        unique_together=("document","chunk_index")  
 
